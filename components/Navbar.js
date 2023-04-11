c8051cd4514520dd0ad8a6a36c99e6ab6499aecf
JHKIMS <wjdgns5131@gmail.com>
2023-04-12 07:26:51 +0900

    Next.js: CssModule을 이용한 스타일적용

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,20 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import styles from "./Navbar.module.css";
 
-export default function Navbar(){
-    const router = useRouter();
-    return <nav>
-        <Link className="hello" href="/">
-            Home
-        </Link>
-        <Link style={{color: router.pathname === "/about" ? "red":"blue"}} href="/about">
-            About
-        </Link>
+export default function Navbar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <Link
+        href="/"
+        className={`${styles.link} ${
+          router.pathname === "/" ? styles.active : ""
+        }`}
+      >
+        Home
+      </Link>
+      <Link href="/about">About</Link>
     </nav>
+  );
 }
