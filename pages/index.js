86136e592c818b7de35ee4a3a7a0e889477e4faf
JHKIMS <wjdgns5131@gmail.com>
2023-06-06 10:50:56 +0900

    Next.js: Fetching Data 영화API

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
-import Head from "next/head";
+import { useState, useEffect } from "react";
 import Seo from "../components/Seo";
 
-export default function Home(){
+const API_KEY = "b6fc4a5936626921cc603197d59f1095";
+
+export default function Home() {
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const {results} = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
-      <Seo title="Home"/>
-      <h1>Home</h1>
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie)=>(
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
