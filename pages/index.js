1a8df3a2848f4bca8bae0cd0b4ba644bed09c255
JHKIMS <wjdgns5131@gmail.com>
2023-07-28 09:25:13 +0900

    Next.js: CatchAll Url처리+404페이지

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import Seo from "../components/Seo";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -6,15 +5,7 @@ import { useRouter } from "next/router";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
   /*   const [movies, setMovies] = useState();
   useEffect(() => {
@@ -39,13 +30,7 @@ export default function Home({ results }) {
         >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <Link
-            href={{
-              pathname: `/movies/${movie.id}`,
-              query: {
-                title: movie.original_title,
-              },
-            }}
-            as={`/movies/${movie.id}`}
+            href={`/movies/${movie.original_title}/${movie.id}`}
           >
             <a>
               <h4>{movie.original_title}</h4>
